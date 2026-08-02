8273bc26df3f2697f9c8855a592ad5140bb787de
shahbod89s <shirazpours@gmail.com>
2026-08-02 18:57:20 +0330

    Fix media serving

--- config/urls.py
+++ config/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     path("dashboard/", include("dashboard.urls")),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
