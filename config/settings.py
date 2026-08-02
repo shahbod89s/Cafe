591d6a2a1c02f0165865fe89f53915bf8d2a4b51
shahbod89s <shirazpours@gmail.com>
2026-08-02 19:18:28 +0330

    Add ImageKit storage

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'menu',
     'dashboard',
+    'imagekit',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +147,14 @@ LOGIN_URL = "/dashboard/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
 
 LOGOUT_REDIRECT_URL = "/dashboard/login/"
+
+AWS_ACCESS_KEY_ID = os.environ.get("IMAGEKIT_PUBLIC_KEY")
+AWS_SECRET_ACCESS_KEY = os.environ.get("IMAGEKIT_PRIVATE_KEY")
+
+AWS_STORAGE_BUCKET_NAME = "media"
+
+AWS_S3_ENDPOINT_URL = "https://ik.imagekit.io/tafdvsgwf"
+
+AWS_S3_FILE_OVERWRITE = False
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
